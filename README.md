9edaa71c04fcc22137801ee3f7c850903fad39f1
Manos Kounelakis <pkounelios@gmail.com>
2019-07-07 01:19:37 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Instructions
+
+Run `npm instal` to install dependencies and the npm start to run the `api`
+
+# List available wifi networks
+
+Do a `GET` request at `localhost:3000/api/networks/list`
+
+This returns an object like this:
+```
+{
+    "networks":[
+        {ssid:"foo","in_use":"false","signal":87,security:"WPA1 WPA2"},
+        {ssid:"foo2","in_use":"true","signal":56,security:"WPA1 WPA2"}
+    ]
+}
+```
+
+# Connect to a specific wifi
+
+You need to do a `POST` request to `localhost:3000/api/networks/connect`.
+In the body of the request you have to pass 2 values:
+* ssid (The ssid of the network)
+* password
---
+++ routes/networks.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const router = express.Router();
+const {
+    exec
+} = require('child_process');
+
+router.get('/list', (req, res, next) => {
+    exec(' nmcli -f SSID,IN-USE,SIGNAL,SECURITY -t dev wifi list', (err, std, stderr) => {
+        if (err || stderr) {
+            return nexr(err || stderr);
+        }
+        console.log(std)
+        let networks = std.split("\n");
+        networks = networks.map(line => {
+            const parts = line.split(":");
+            return {
+                ssid: parts[0],
+                in_use: parts[1] == "*",
+                signal: parts[2],
+                security: parts[3]
+            }
+        }).filter(e => e.ssid.length > 0);
+        res.json({
+            networks
+        });
+    });
+});
+
+router.post('/connect', (req, res, next) => {
+    const {
+        ssid,
+        password
+    } = req.body;
+    const commandString = `nmcli device wifi connect "${ssid}" password ${password}`;
+    console.log(commandString)
+    exec(commandString, (err, std, stderr) => {
+        if (err || stderr) {
+            return next(err || stderr);
+        }
+        console.log({
+            err,
+            std,
+            stderr
+        });
+        res.json({
+            std
+        });
+    })
+});
+module.exports = router;
