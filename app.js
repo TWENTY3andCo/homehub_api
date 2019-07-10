563648e72c0a9b2960f55aafa1eb81bfaccd39b8
Manos Kounelakis <pkounelios@gmail.com>
2019-07-10 21:46:02 +0300

    removed jade

--- app.js
+++ app.js
@@ -1,17 +1,10 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
+const createError = require('http-errors');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const app = express();
 const cors = require('cors');
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,10 +12,7 @@ app.use(express.urlencoded({
   extended: false
 }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/api/networks', require('./routes/networks'));
 app.use('/api/devices', require('./routes/devices'));
 // catch 404 and forward to error handler
@@ -32,11 +22,10 @@ app.use(function (req, res, next) {
 
 // error handler
 app.use(function (err, req, res, next) {
+  console.log(err);
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.json(err);
 });
