dae60fc84593560872395968b6a093a91ba6d769
Manos Kounelakis <pkounelios@gmail.com>
2019-07-11 14:00:51 +0300

    configure access for swaggerhub

--- app.js
+++ app.js
@@ -4,7 +4,20 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const app = express();
 const cors = require('cors');
-
+const allowedOrigins = ["https://app.swaggerhub.com"]
+const corsOptions = {
+  //origin: 'https://app.swaggerhub.com'
+  origin: function (origin, callback) {
+    console.log('origin ', origin)
+    // if origin is undefined it means we use this on localhost
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
+  credentials: true
+}
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -12,7 +25,7 @@ app.use(express.urlencoded({
   extended: false
 }));
 app.use(cookieParser());
-app.use(cors());
+app.use(cors(corsOptions));
 app.use('/api/networks', require('./routes/networks'));
 app.use('/api/devices', require('./routes/devices'));
 // catch 404 and forward to error handler
